9d1846dfa0e8af753e3582970bc776e7b2d6e897
Rupesh Nipanikar <rupeshnipanikar4@gmail.com>
2025-03-21 14:27:44 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     try {
-        const response = await fetch('http://localhost:5000/', {
+        const response = await fetch('https://codex-t76u.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ prompt: userInput })
